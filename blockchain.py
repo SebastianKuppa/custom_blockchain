00e127e93c4460517a46c38fc078857fe3ed33c8
sebastian_kup <sebastian.kuppa@web.de>
2022-11-23 16:56:30 +0800

    added PoW functions

--- blockchain.py
+++ blockchain.py
@@ -43,3 +43,28 @@ class BlockChain:
         block_string = json.dumps(block, sort_keys=True).encode()
 
         return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work algorithm. Find a number p' where hash(pp') contains 4 leading zeroes, where p
+        is the previous p'
+        p: previous proof, p' new proof
+        :param last_proof:previous proof
+        :return: new proof
+        """
+        proof = 0
+        while not self.valid_proof(last_proof, proof):
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Analyzing hash for the first 4 chars contain zero
+        :param last_proof: <int>
+        :param proof: <int>
+        :return: <bool>
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == '0000'
