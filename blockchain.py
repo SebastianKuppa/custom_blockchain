82522a7c589e1ebf3252132766d82dc527144e81
sebastian_kup <sebastian.kuppa@web.de>
2022-11-24 17:33:33 +0800

    added mining function

--- blockchain.py
+++ blockchain.py
@@ -83,7 +83,29 @@ blockchain = BlockChain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return 'We will mine a new block'
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # node identifier receives reward for finding new proof
+    # sender address is '0' to show that this coin was mined
+    blockchain.new_transaction(
+        sender='0',
+        recipient=node_identifier,
+        amount=1,
+    )
+    # add new block to chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': 'New Block forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
